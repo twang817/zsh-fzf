f28a7ff9b518b24798043d895ac81a4ae3f6409e
Tommy Wang <twang@august8.net>
2019-03-28 12:57:59 -0500

    fix issue when not using brew that it always clones the repo

--- fzf.plugin.zsh
+++ fzf.plugin.zsh
@@ -1,11 +1,16 @@
+if [[ -f ~/.fzf.zsh ]]; then
+    source ~/.fzf.zsh
+    return
+fi
+
 FZF=$(which fzf) || FZF=
 
 if [[ -z "$FZF" ]]; then
     git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
     ~/.fzf/install --key-bindings --completion --no-update-rc &> /dev/null
-elif [[ ! -f ~/.fzf.zsh ]]; then
-  INSTALL=$(cd $(dirname $(readlink -f $FZF))/..; pwd)/install
-  $INSTALL --key-bindings --completion --no-update-rc &> /dev/null
+else
+    INSTALL=$(cd $(dirname $(readlink -f $FZF))/..; pwd)/install
+    $INSTALL --key-bindings --completion --no-update-rc &> /dev/null
 fi
-
+  
 source ~/.fzf.zsh
